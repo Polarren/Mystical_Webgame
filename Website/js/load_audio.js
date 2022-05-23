9f36052c1441862886ee0ee154c9191d0c742e29
Chang Liu <changl12@illinois.edu>
2022-05-23 16:19:21 -0500

    now only one audio play function

--- Website/js/load_audio.js
+++ Website/js/load_audio.js
@@ -1,28 +1,34 @@
-function play1() {
-    var audio_stop1 = document.getElementById("audio2");
-    audio_stop1.pause();
-    var audio_stop2 = document.getElementById("audio3");
-    audio_stop2.pause();
-    var audio = document.getElementById("audio1");
-    audio.currentTime=0;
-    audio.play();
-}
-function play2() {
-    var audio_stop1 = document.getElementById("audio1");
-    audio_stop1.pause();
-    var audio_stop2 = document.getElementById("audio3");
-    audio_stop2.pause();
-    var audio = document.getElementById("audio2");
-    audio.currentTime=0;
-    audio.play();
-}
+function play_audio(audio_id,audio_src) {
+    // var audio_stop1 = document.getElementById("audio2");
+    // audio_stop1.pause();
+    // var audio_stop2 = document.getElementById("audio3");
+    // audio_stop2.pause();
+    var audio = document.getElementById(audio_id);
+    audio.src = audio_src;
 
-function play3() {
-    var audio_stop1 = document.getElementById("audio1");
-    audio_stop1.pause();
-    var audio_stop2 = document.getElementById("audio2");
-    audio_stop2.pause();
-    var audio = document.getElementById("audio3");
+    // <audio controls="controls">
+    //     <source src="audios/trial_room_1.mp3" type="audio/mpeg">
+    // Your browser does not support the audio element.
+    // </audio>
     audio.currentTime=0;
     audio.play();
 }
+// function play2() {
+//     var audio_stop1 = document.getElementById("audio1");
+//     audio_stop1.pause();
+//     var audio_stop2 = document.getElementById("audio3");
+//     audio_stop2.pause();
+//     var audio = document.getElementById("audio2");
+//     audio.currentTime=0;
+//     audio.play();
+// }
+
+// function play3() {
+//     var audio_stop1 = document.getElementById("audio1");
+//     audio_stop1.pause();
+//     var audio_stop2 = document.getElementById("audio2");
+//     audio_stop2.pause();
+//     var audio = document.getElementById("audio3");
+//     audio.currentTime=0;
+//     audio.play();
+// }
